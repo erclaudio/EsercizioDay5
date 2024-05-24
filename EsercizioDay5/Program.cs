6fa4b78cd1a707fae45a60ddb7f5b94478c0d50d
erclaudio <erfattone@gmail.com>
2024-05-24 14:22:07 +0100

    Add project files.

---
+++ EsercizioDay5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioDay5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Inserici un numero decimale: ");
+            //string numero = Console.ReadLine();
+            //float x;
+            //float.TryParse(numero, out x);
+            ////Console.WriteLine("Il tuo numero: '{0}' convertito diventa: '{1}'", numero,(int) double.Parse(numero));
+            //Console.WriteLine("Il tuo numero: '{0}' convertito diventa: '{1}'", numero, x);
+            //Console.Read();
+
+            
+            /*
+            Console.WriteLine("Inserisici un numero decimale: ");
+            float numero = (float)Convert.ToDouble(Console.ReadLine());
+            int converted = Convert.ToInt32(numero);
+            Console.WriteLine("Il tuo numero: '{0}', convertito diventa: '{1}'", numero, converted);
+            Console.ReadLine();
+            */
+        }
+    }
+}
